41dc494ae3c41e005fd3f4b26825dec4d29b9155
itmanbanana <itmanbanana@gmail.com>
2026-06-22 20:13:32 -0400

    Client: Add message board
    - Query to backend for message list
    - Display messages on MessageBoard page
    - Basic styling

--- client/app/components/MessageBoardLayout.tsx
+++ client/app/components/MessageBoardLayout.tsx
@@ -1,12 +1,71 @@
-import { heading1Style } from "~/styles/styleTemplates";
+import { heading1Style, heading2Style, inputStyle, messageItemStyle, messageListStyle, submitButtonStyle } from "~/styles/styleTemplates";
 import BackHomeButton from "./BackHomeButton";
+import { Form } from "react-router";
+import { SERVER_URL } from "~/routes/Home";
+import type { MessageType } from "~/types/MessageType.type";
+import { useEffect, useState } from "react";
+
+const loadMessages = async () => {
+  const res = await fetch(`${SERVER_URL}/api/messages/get-messages`, {
+    method: 'get',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+  });
+  if (!res.ok) {
+    const error = await res.json();
+    console.error(error);
+    return error;
+  }
+
+  const data = await res.json();
+  return data;
+};
 
 const MessageBoardLayout = ({ authUsername }: { authUsername: string | null; }) => {
+  const [messageList, setMessageList] = useState<MessageType[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const asyncLoadMessages = async () => {
+      setMessageList(await loadMessages());
+      setLoading(false);
+    };
+
+    if (loading) asyncLoadMessages();
+  }, [messageList, loading]);
+
+  console.log(messageList);
 
   return (
     <>
       <h1 className={heading1Style}>Message Board</h1>
-      <p>Welcome {authUsername}</p>
+      <h2 className={heading2Style}>Welcome {authUsername}</h2>
+      <Form className={`flex justify-around md:justify-center md:gap-4`}>
+        <div className="">
+          <label htmlFor="newMessage">Enter message: </label>
+          <input className={inputStyle} type="text" id="newMessage" name="newMessage" />
+        </div>
+        <button className={submitButtonStyle} type="submit">Send</button>
+      </Form>
+      <ul className={messageListStyle}>
+        {
+          loading ? <li>Loading...</li> :
+            messageList.map(message => {
+              return (
+                <li className={messageItemStyle} key={message.posted}>
+                  <p>{message.authorname}: {message.message}</p>
+                  <Form className="flex gap-4">
+                    {/* TODO: Only admins should have this button enabled */}
+                    <button className={submitButtonStyle} type="submit">Delete</button>
+                  </Form>
+                </li>
+              );
+            })
+        }
+      </ul>
       <BackHomeButton />
     </>
   );
--- client/app/styles/styleTemplates.ts
+++ client/app/styles/styleTemplates.ts
@@ -1,7 +1,10 @@
 export const homeLinkStyle = `p-2 rounded-lg bg-gray-500`;
 export const heading1Style = `p-8 flex justify-center text-3xl font-semibold`;
+export const heading2Style = `p-4 flex justify-center text-2xl font-semibold`;
 export const formRowStyle = `grid grid-rows-2 justify-center mb-4`;
 export const inputStyle = `border-1 border-gray-500 rounded-lg p-1 text-sm`;
-export const submitButtonStyle = `p-2 rounded-lg bg-gray-500 hover:bg-gray-800`;
+export const submitButtonStyle = `p-1 rounded-lg bg-gray-500 hover:bg-gray-800`;
 export const errorStyle = `pb-4 flex justify-center text-red-400 text-semibold text-md`;
 export const backHomeButtonStyle = `${homeLinkStyle} flex justify-center mt-2 mx-auto`;
+export const messageListStyle = `m-2 p-4 flex flex-col justify-center border-gray-500 border-1 rounded-lg`;
+export const messageItemStyle = `flex items-center gap-4`;
